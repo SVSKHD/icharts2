eacaea8975d09d23c0bf548298d124f4e641f4f6
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2021-05-30 19:32:04 +0530

    changes push

--- src/pages/ivp.js
+++ src/pages/ivp.js
@@ -1,7 +1,115 @@
+import {useEffect , useState} from "react"
+import Layout from "../components/Layout/Layout"
+import DataTable from 'react-data-table-component';
+import {FaSortAlphaUp} from "react-icons/fa"
+
+const columns = [
+    {
+      name: 'Symbol',
+      selector: '0',
+      sortable: true,
+    },
+    {
+      name: 'Price',
+      selector: '1',
+      sortable: true,
+    },
+    {
+      name: 'Price Chnage',
+      selector: '2',
+      sortable: true,
+      
+    },
+    {
+      name: 'Price Change (%)',
+      selector: '3',
+      sortable: true,
+      
+    },
+    {
+      name: 'Total OI',
+      selector: '4',
+      sortable: true,
+      
+    },
+    {
+      name: 'Total OI Chnage',
+      selector: '5',
+      sortable: true,
+      
+    },
+    {
+      name: 'Total OI Chnage (%)',
+      selector: '6',
+      sortable: true,
+     
+    },
+    {
+      name: 'PCR',
+      selector: '7',
+      sortable: true,
+      
+    },
+    {
+      name: 'IV',
+      selector: '8',
+      sortable: true,
+      
+    },
+    {
+      name: 'IV Range (1yr)',
+      selector: '9',
+      sortable: true,
+     
+    },
+    {
+      name: 'HV(10/20/30 Days)',
+      selector: '10',
+      sortable: true,
+    },
+    {
+      name: 'IVR',
+      selector: '11',
+      sortable: true,
+    },
+    {
+      name: 'IVP',
+      selector: '12',
+      sortable: true,
+    },
+  ];
+  
+
+
+
+
+
 const IVP = () =>{
+    const [data , setData] = useState([])
+
+    
+       useEffect(()=>{
+        setInterval(()=>{
+          const url = "https://options.icharts.in/opt/hcharts/stx8req/php/getDataForIVR_IVP_Report_BhavCopy_for_test.php"
+          fetch(url).then(resp=>resp.json())
+          .then(resp=>setData(resp.aaData))
+          console.log(data)
+        },1000)
+        },[])
+
+
+
     return(
         <div>
+            <Layout>
             <h1>Hello IVP</h1>
+            <DataTable
+            columns={columns}
+            data={data}
+            highlightOnHover
+            sortIcon={<FaSortAlphaUp/>}
+            />
+            </Layout>
         </div>
     )
     }
--- src/pages/ivp2.js
+++ src/pages/ivp2.js
@@ -1,8 +1,51 @@
+import {useEffect , useState} from "react"
+import Layout from "../components/Layout/Layout"
+import DataTable from 'react-data-table-component';
+
+import {FaSortAlphaUp} from "react-icons/fa"
+
+const columns = [
+    {
+      name: 'Name',
+      selector: '0',
+      sortable: true,
+    },
+    {
+      name: 'Type',
+      selector: '1',
+      sortable: true,
+    },
+    {
+      name: 'Future OI',
+      selector: '2',
+      sortable: true,
+      
+    },
+]
+
 const IVR2 = () =>{
+    const [data , setData] = useState([])
+
+    useEffect(()=>{
+    const url ="https://options.icharts.in/opt/hcharts/stx8req/php/getDataForFuturesScan_for_react_test.php"
+    fetch(url).then(resp=>resp.json())
+    .then(resp=>setData(resp.aaData))
+    console.log(data)
+    },[])
+
+
     return(
         <div>
-            <h1>Hello</h1>
-        </div>
+            <Layout>
+            <h1>Hello this Future Scan Page</h1>
+            <DataTable
+            columns={columns}
+            data={data}
+            highlightOnHover
+            sortIcon={<FaSortAlphaUp/>}
+            />
+            </Layout>
+            </div>
     )
     }
     export default IVR2
